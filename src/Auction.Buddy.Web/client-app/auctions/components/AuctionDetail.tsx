b8d929e0783308ed33b0d45c345cbee645cfdb10
Bryce Klinker <bryce.klinker@gmail.com>
2019-07-06 00:20:27 -0500

    Working acceptance test for creating an auction

--- src/Auction.Buddy.Web/client-app/auctions/components/AuctionDetail.tsx
+++ src/Auction.Buddy.Web/client-app/auctions/components/AuctionDetail.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
+import { format } from 'date-fns';
 
 import {AuctionDto} from "../../common/store/dtos/auction-dto";
 
 interface Props {
-    auction: AuctionDto;
+    auction?: AuctionDto;
 }
 
 export function AuctionDetail({auction}: Props) {
+    const auctionDate = auction 
+        ? format(auction.auctionDate, 'MM/DD/YYYY')
+        : '';
+    
     return (
         <div>
-            <div data-testid="auction-name">{auction.name}</div>
-            <div data-testid="auction-date">{auction.auctionDate}</div>
+            <div data-testid="auction-name">{auction ? auction.name : null}</div>
+            <div data-testid="auction-date">{auctionDate}</div>
         </div>
     )
 }
--- src/Auction.Buddy.Web/client-app/auctions/containers/AuctionDetailContainer.spec.tsx
+++ src/Auction.Buddy.Web/client-app/auctions/containers/AuctionDetailContainer.spec.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
-import {createMemoryHistory} from "history";
+import {createMemoryHistory, History} from "history";
+import {Store} from "redux";
+import {cleanup} from "@testing-library/react";
+import {ConnectedRouter} from "connected-react-router";
+import {Route} from "react-router";
 
 import {createTestingStore} from "../../testing/testing-store";
 import {createAuctionSuccessAction} from "../../common/store/actions/auction-actions";
 import {AuctionDto} from "../../common/store/dtos/auction-dto";
 import {renderWithStore} from "../../testing/render-with-store";
 import {AuctionDetailContainer} from "./AuctionDetailContainer";
-import {cleanup} from "@testing-library/react";
-import {ConnectedRouter} from "connected-react-router";
-import {Route} from "react-router";
+import {AppState} from "../../app-state";
 
 describe('AuctionDetailContainer', () => {
     it('should show auction info', () => {
@@ -22,12 +24,12 @@ describe('AuctionDetailContainer', () => {
         const {getByTestId} = renderWithStoreAndRouter(history, store);
 
         expect(getByTestId('auction-name')).toHaveTextContent('john');
-        expect(getByTestId('auction-date')).toHaveTextContent('2019-04-21');
+        expect(getByTestId('auction-date')).toHaveTextContent('04/21/2019');
     });
 
     afterEach(() => cleanup());
 
-    function renderWithStoreAndRouter(history, store) {
+    function renderWithStoreAndRouter(history: History, store: Store<AppState>) {
         return renderWithStore(
             <ConnectedRouter history={history}>
                 <Route path={'/auction-detail/:id'} component={AuctionDetailContainer}/>
