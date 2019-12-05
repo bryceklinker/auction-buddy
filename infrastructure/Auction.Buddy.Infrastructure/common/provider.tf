e3ed65c47870eadccb140d01f4d213985363a59c
Bryce Klinker <bryce.klinker@gmail.com>
2019-12-04 23:28:17 -0600

    Using terraform to set variables instead

--- infrastructure/Auction.Buddy.Infrastructure/common/provider.tf
+++ infrastructure/Auction.Buddy.Infrastructure/common/provider.tf
@@ -1,3 +1,18 @@
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
 
 terraform {
   required_version = "0.12.17"
@@ -5,4 +20,13 @@ terraform {
   required_providers {
     azurerm = "=1.36.0"
   }
+}
+
+provider "azurerm" {
+  version = "=1.36.0"
+  
+  client_id = var.client_id
+  client_secret = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
 }
