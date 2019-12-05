68a57b650954b7268468cff6ff7da3186310b7d0
Bryce Klinker <bryce.klinker@gmail.com>
2019-12-04 23:36:55 -0600

    More terraform fiddling

--- infrastructure/Auction.Buddy.Infrastructure/common/provider.tf
+++ infrastructure/Auction.Buddy.Infrastructure/common/provider.tf
@@ -1,32 +1,7 @@
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-
-variable "subscription_id" {
-  type = string
-}
-
 terraform {
   required_version = "0.12.17"
-  
-  required_providers {
-    azurerm = "=1.36.0"
-  }
 }
 
 provider "azurerm" {
   version = "=1.36.0"
-  
-  client_id = var.client_id
-  client_secret = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id = var.tenant_id
 }
