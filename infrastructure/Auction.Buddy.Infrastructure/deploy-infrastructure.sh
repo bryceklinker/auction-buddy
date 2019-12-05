d904aae62bb0b86098df3ae87cbc4f2877302f08
Bryce Klinker <bryce.klinker@gmail.com>
2019-12-04 22:27:44 -0600

    Adding the start of deploying infrastructure using terraform

---
+++ infrastructure/Auction.Buddy.Infrastructure/deploy-infrastructure.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ex
+
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+
+sudo apt-get update
+
+sudo apt-get install wget unzip
+
+sudo wget ${TERRAFORM_URL}
+
+unzip terraform_0.12.7_linux_amd64.zip
+
+sudo mv terraform /usr/local/bin/
+
+pushd ./prod
+
+terraform init
+terraform plan -out=./tfplan.plan
+
+popd
